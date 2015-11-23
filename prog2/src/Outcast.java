ed7134bf2d1852ae5d0a43133546acbdc6fac9d1
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2015-11-22 20:19:05 -0500

    added base class

---
+++ prog2/src/Outcast.java
@@ -0,0 +1,20 @@
+public class Outcast
+{
+    //constructor takes a WordNet object
+    public Outcast(WordNet wordnet)
+    {
+
+    }
+
+    //given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns)
+    {
+        throw new NullPointerException();
+    }
+
+    //see test client below
+    public static void main(String[] args)
+    {
+
+    }
+}
