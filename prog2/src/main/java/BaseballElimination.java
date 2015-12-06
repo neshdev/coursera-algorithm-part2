83a979d509023ac8e021d412db1751b1c7498081
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2015-12-06 15:51:23 -0500

    simple parsing of baseball elim file

--- prog2/src/main/java/BaseballElimination.java
+++ prog2/src/main/java/BaseballElimination.java
@@ -1,56 +1,139 @@
+import java.util.HashMap;
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
+
+// immutable data type BaseballElimination that represents a sports division and determines which teams are mathematically eliminated
 public class BaseballElimination
 {
+    private Queue<String> teams;
+    private HashMap<String, Integer> teamToIndex;
+    private int[][] gamesRemaining;
+    private int[] wins;
+    private int[] losses;
+    private int[] remaining;
+
+    private void validateTeam(String team)
+    {
+        if (team == null) {
+            throw new IllegalArgumentException("team cannot be null");
+        }
+
+        if (!teamToIndex.containsKey(team)) {
+            throw new IllegalArgumentException("Not a valid team");
+        }
+    }
+
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename)
     {
+        In in = new In(filename);
+        int length = in.readInt();
 
+        teamToIndex = new HashMap<String, Integer>();
+        teams = new Queue<String>();
+        wins = new int[length];
+        losses = new int[length];
+        remaining = new int[length];
+        gamesRemaining = new int[length][length];
+        
+        for (int i = 0; i < length; i++) {
+            String name = in.readString();
+            int win = in.readInt();
+            int loss = in.readInt();
+            int remain = in.readInt();
+
+            for (int j = 0; j < length; j++) {
+                gamesRemaining[i][j] = in.readInt();
+            }
+            
+            teamToIndex.put(name, i);
+            teams.enqueue(name);
+            wins[i] = win;
+            losses[i] = loss;
+            remaining[i] = remain;
+        }
     }
 
     // number of teams
     public int numberOfTeams()
     {
-        throw new NullPointerException();
+        return teams.size();
     }
 
     // all teams
     public Iterable<String> teams()
     {
-        throw new NullPointerException();
+        return teams;
     }
 
     // number of wins for given team
     public int wins(String team)
     {
-        throw new NullPointerException();
+        validateTeam(team);
+        Integer i = teamToIndex.get(team);
+        return wins[i];
     }
 
     // number of losses for given team
     public int losses(String team)
     {
-        throw new NullPointerException();
+        validateTeam(team);
+        Integer i = teamToIndex.get(team);
+        return losses[i];
     }
 
     // number of remaining games for given team
     public int remaining(String team)
     {
-        throw new NullPointerException();
+        validateTeam(team);
+        Integer i = teamToIndex.get(team);
+        return remaining[i];
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2)
     {
-        throw new NullPointerException();
+        validateTeam(team1);
+        validateTeam(team2);
+
+        Integer i = teamToIndex.get(team1);
+        Integer j = teamToIndex.get(team2);
+        return gamesRemaining[i][j];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team)
     {
+        validateTeam(team);
+
         throw new NullPointerException();
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team)
     {
+        validateTeam(team);
+
         throw new NullPointerException();
+    }
+
+    public static void main(String[] args)
+    {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
     }
 }
--- prog2/src/test/java/BaseballEliminationTest.java
+++ prog2/src/test/java/BaseballEliminationTest.java
@@ -2,14 +2,67 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class BaseballEliminationTest
 {
 
     @Test
-    public void test()
+    public void LoadFile()
     {
-        fail("Not yet implemented");
+        String filename = "C:/coursera/Algorithms, Part II/assignments/baseball/teams4.txt";
+        BaseballElimination be = new BaseballElimination(filename);
+        assertNotNull(be);
+    }
+
+    private static int TEAMS4_COUNT = 4;
+    private static String[] TEAMS4_TEAMS = new String[] { "Atlanta", "Philadelphia", "New_York",
+            "Montreal" };
+    private static int[] TEAM4_WINS = new int[] { 83, 80, 78, 77 };
+    private static int[] TEAM4_LOSSES = new int[] { 71, 79, 78, 82 };
+    private static int[] TEAM4_REMAIN = new int[] { 8, 3, 6, 3 };
+    private static int TEAM4_ATLvsPHL_REMAIN = 1; 
+    private static int TEAM4_ATLvsNYC_REMAIN = 6;
+    private static int TEAM4_ATLvsMON_REMAIN = 1;
+    
+
+    @Test
+    public void validateSize()
+    {
+        String filename = "C:/coursera/Algorithms, Part II/assignments/baseball/teams4.txt";
+        BaseballElimination be = new BaseballElimination(filename);
+        assertEquals(TEAMS4_COUNT, be.numberOfTeams());
+    }
+
+    @Test
+    public void validateTeams()
+    {
+        String filename = "C:/coursera/Algorithms, Part II/assignments/baseball/teams4.txt";
+        BaseballElimination be = new BaseballElimination(filename);
+        int i = 0;
+        for (String team : be.teams()) {
+            assertEquals(TEAMS4_TEAMS[i++], team);
+        }
+    }
+    
+    @Test
+    public void validateWinsLossesAndRemaining(){
+        String filename = "C:/coursera/Algorithms, Part II/assignments/baseball/teams4.txt";
+        BaseballElimination be = new BaseballElimination(filename);
+        
+        for (int i = 0; i < TEAMS4_TEAMS.length; i++) {
+            String team = TEAMS4_TEAMS[i];
+            assertEquals(TEAM4_WINS[i], be.wins(team));
+            assertEquals(TEAM4_LOSSES[i], be.losses(team));
+            assertEquals(TEAM4_REMAIN[i], be.remaining(team));
+        }
+    }
+    
+    @Test
+    public void validateAgainst(){
+        String filename = "C:/coursera/Algorithms, Part II/assignments/baseball/teams4.txt";
+        BaseballElimination be = new BaseballElimination(filename);
+        assertEquals(TEAM4_ATLvsPHL_REMAIN, be.against("Atlanta", "Philadelphia"));
+        assertEquals(TEAM4_ATLvsNYC_REMAIN, be.against("Atlanta", "New_York"));
+        assertEquals(TEAM4_ATLvsMON_REMAIN, be.against("Atlanta", "Montreal"));
     }
 
 }
