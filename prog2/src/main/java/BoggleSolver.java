9ed0802ad4d8283a79f0c92fbc20d1db06f4548c
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2015-12-13 21:23:42 -0500

    moved Trie to internal class of boggle solver

--- prog2/src/main/java/BoggleSolver.java
+++ prog2/src/main/java/BoggleSolver.java
@@ -4,60 +4,63 @@ import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdOut;
 
-class Trie<Value>
-{
-    private static final int R = 256;
-    
-    private static class Node
-    {
-        private Object val;
-        private Node[] next = new Node[R];
-    }
-    
-    public Trie(){
-        
-    }
-    
-    private Node root;
-    
-    public void put(String key, Value value){
-        root = put(root, key, value, 0 );
-    }
-    
-    private Node put(Node x, String key, Value val, int d){
-        if ( x == null) { x = new Node();}
-        if ( d == key.length()) { x.val = val; return x; }
-        char c = key.charAt(d);
-        x.next[c] = put(x.next[c], key, val, d+1);
-        return x;
-    }
-    
-    public boolean contains(String key){
-        return get(key) != null;
-    }
-    
-    public Value get(String key){
-        Node x = get(root, key, 0);
-        if (x == null) return null;
-        return (Value)x.val;
-    }
-    
-    private Node get(Node x, String key, int d){
-        if ( x == null) return x;
-        if ( d == key.length() ) return x;
-        char c = key.charAt(d);
-        return get(x.next[c], key, d+1);
-    }
-    
-    public boolean matchPrefix(String prefix){
-        Node x = get(root, prefix, 0);
-        return ( x != null); 
-    }
-}
+
 
 
 public class BoggleSolver
 {
+    private static class Trie<Value>
+    {
+        private static final int R = 256;
+        
+        private static class Node
+        {
+            private Object val;
+            private Node[] next = new Node[R];
+        }
+        
+        public Trie(){
+            
+        }
+        
+        private Node root;
+        
+        public void put(String key, Value value){
+            root = put(root, key, value, 0 );
+        }
+        
+        private Node put(Node x, String key, Value val, int d){
+            if ( x == null) { x = new Node();}
+            if ( d == key.length()) { x.val = val; return x; }
+            char c = key.charAt(d);
+            x.next[c] = put(x.next[c], key, val, d+1);
+            return x;
+        }
+        
+        public boolean contains(String key){
+            return get(key) != null;
+        }
+        
+        public Value get(String key){
+            Node x = get(root, key, 0);
+            if (x == null) return null;
+            return (Value)x.val;
+        }
+        
+        private Node get(Node x, String key, int d){
+            if ( x == null) return x;
+            if ( d == key.length() ) return x;
+            char c = key.charAt(d);
+            return get(x.next[c], key, d+1);
+        }
+        
+        public boolean matchPrefix(String prefix){
+            Node x = get(root, prefix, 0);
+            return ( x != null); 
+        }
+    }
+    
+    
     private Trie<Integer> words;
 
     // Initializes the data structure using the given array of strings as the dictionary.
