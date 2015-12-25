813ee4ddd631b579b62bed454aaf2c6bf1c13cda
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2015-12-25 00:52:46 -0500

    implement movetofront and decode methods

--- prog2/src/main/java/BurrowsWheeler.java
+++ prog2/src/main/java/BurrowsWheeler.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
@@ -15,31 +18,73 @@ public class BurrowsWheeler {
         int N = s.length();
         CircularSuffixArray csa = new CircularSuffixArray(s);
         for (int i = 0; i < N; i++) {            
-            int index = csa.index(i);
-            if ( index == 0) {
-                BinaryStdOut.write(index);
+            int sortIndex = csa.index(i);
+            if ( sortIndex == 0) {
+                BinaryStdOut.write(i);
                 break;
             }
         }
         
         for (int i = 0; i < N; i++) {            
-            int index = csa.index(i);
-            int charIndex = index - 1;
+            int sortIndex = csa.index(i);
+            int charIndex = sortIndex - 1;
             if (charIndex != -1){
                 char c = s.charAt(charIndex);
-                BinaryStdOut.write( c);
+                BinaryStdOut.write(c);
             } else {
                 char c = s.charAt(N - 1);
-                BinaryStdOut.write( c);
+                BinaryStdOut.write(c);
             }
         }
+        
+        BinaryStdOut.flush();
         
     }
 
     // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
     public static void decode()
     {
+        int first = BinaryStdIn.readInt();
+        String s = BinaryStdIn.readString();
+                
+        char[] tail = s.toCharArray();
+        char[] head = s.toCharArray();
+        Arrays.sort(head);
+        int[] next = next(tail, head);
+
+        int index = first;
+        for (char ignored : tail) {
+            BinaryStdOut.write(head[index]);
+            index = next[index];
+        }
+
+        BinaryStdOut.flush();
         
+    }
+    
+    private static int R = 256;
+    
+    private static int[] next(char[] tail, char[] head){
+        int[] searchStart = new int[R];
+
+        int[] next = new int[tail.length];
+        for (int i = 0; i < tail.length; i++) {
+            char c = tail[i];
+            int index = search(head, searchStart[c], c);
+            searchStart[c] = index + 1;
+            next[index] = i;
+        }
+
+        return next;
+    }
+    
+    private static int search(char[] characters, int from, char c) {
+        for (int i = from; i < characters.length; i++) {
+            if (characters[i] == c) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // if args[0] is '-', apply Burrows-Wheeler encoding
--- prog2/src/main/java/MoveToFront.java
+++ prog2/src/main/java/MoveToFront.java
@@ -1,14 +1,56 @@
+import java.util.LinkedList;
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class MoveToFront {
-    // apply move-to-front encoding, reading from standard input and writing to standard output
-    public static void encode()
-    {
-        
+    /**
+     * apply move-to-front encoding,
+     * reading from standard input and writing to standard output
+     */
+    public static void encode() {
+        LinkedList<Character> alphabet = new LinkedList<Character>();
+        for (char c = 0; c < 256; c++) {
+            alphabet.addLast(c);
+        }
+
+        char c;
+        while (!BinaryStdIn.isEmpty()) {
+            c = BinaryStdIn.readChar();
+            // Get index
+            int index = alphabet.indexOf(c);
+            alphabet.remove(index);
+
+            // Output
+            BinaryStdOut.write((char) index);
+
+            // Adjust alphabet
+            alphabet.addFirst(c);
+        }
+
+        BinaryStdOut.flush();
     }
 
-    // apply move-to-front decoding, reading from standard input and writing to standard output
-    public static void decode()
-    {
-        
+    /**
+     * apply move-to-front decoding,
+     * reading from standard input and writing to standard output
+     */
+    public static void decode() {
+        LinkedList<Character> alphabet = new LinkedList<Character>();
+        for (char c = 0; c < 256; c++) {
+            alphabet.addLast(c);
+        }
+
+        int index;
+        while (!BinaryStdIn.isEmpty()) {
+            index = BinaryStdIn.readChar();
+            char c = alphabet.remove(index);
+            BinaryStdOut.write(c);
+
+            // Adjust alphabet
+            alphabet.addFirst(c);
+        }
+
+        BinaryStdOut.flush();
     }
 
     // if args[0] is '-', apply move-to-front encoding
