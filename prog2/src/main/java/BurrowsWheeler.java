747d241cf4a92e8578eb1f2d54b96440f9b995ad
Dhinesh Kumar Devanathan <dhineshdevanathan@gmail.com>
2015-12-24 22:50:42 -0500

    implemented encode method

--- prog2/src/main/java/BurrowsWheeler.java
+++ prog2/src/main/java/BurrowsWheeler.java
@@ -1,7 +1,38 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class BurrowsWheeler {
     // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
     public static void encode()
     {
+        StringBuilder sb = new StringBuilder();
+        while (!BinaryStdIn.isEmpty())
+        {
+            char c = BinaryStdIn.readChar();
+            sb.append(c);
+        }
+        String s = sb.toString();
+        int N = s.length();
+        CircularSuffixArray csa = new CircularSuffixArray(s);
+        for (int i = 0; i < N; i++) {            
+            int index = csa.index(i);
+            if ( index == 0) {
+                BinaryStdOut.write(index);
+                break;
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {            
+            int index = csa.index(i);
+            int charIndex = index - 1;
+            if (charIndex != -1){
+                char c = s.charAt(charIndex);
+                BinaryStdOut.write( c);
+            } else {
+                char c = s.charAt(N - 1);
+                BinaryStdOut.write( c);
+            }
+        }
         
     }
 
@@ -15,6 +46,14 @@ public class BurrowsWheeler {
     // if args[0] is '+', apply Burrows-Wheeler decoding
     public static void main(String[] args)
     {
+        if ( args.length == 0) throw new IllegalArgumentException("args should be greater than 1");
+        String type = args[0];
+        if ( type == "+"){
+            decode();
+        }
         
+        if ( type == "-"){
+            encode();
+        }
     }
 }
